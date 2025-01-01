d66fd0fc9937d38b8754d48d5c71879fa6556f39
netcoffeecat-2 <70847177+netcoffeecat-2@users.noreply.github.com>
2025-01-01 10:31:17 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -26,6 +26,7 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-udpxy-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
